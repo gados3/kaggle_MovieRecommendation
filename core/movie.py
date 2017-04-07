7e0d9b70c34f67232df89afb060b65af65a36137
gados3 <gael.dostie.1@ulaval.ca>
2017-04-07 00:25:59 -0400

    used regex to get year out of title

--- core/movie.py
+++ core/movie.py
@@ -1,3 +1,4 @@
+import re
 from core.data_types import Genre
 
 
@@ -5,8 +6,10 @@ class Movie:
 
     def __init__(self, movie_id: int, title: str, genres: str):
         self.id = movie_id
-        self.title = title
+        self.title = re.sub(r"\(\d{4}\)", '', title)
         self.genres = self.parse_genres(genres)
+        self.year = re.search(r"\(\d{4}\)", title).group(
+            0).strip('(').strip(')')
 
     def parse_genres(self, genres_string: str):
         return list(map(lambda genre: Genre(genre), genres_string.split('|')))
@@ -19,7 +22,8 @@ class Movie:
                                 ' / '.join(map(lambda genre: str(genre), self.genres)))
 
     def itemize(self):
-        movie_attributes = [("movie_title", self.title)]
+        movie_attributes = [("movie_title", self.title),
+                            ("movie_year", self.year)]
         attr_genres = [("movie_genre", g) for g in self.genres]
         movie_attributes.extend(attr_genres)
         return movie_attributes
